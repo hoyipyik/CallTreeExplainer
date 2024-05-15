684cbe896775fdc7f6406b9246c548aa50f79f83
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-15 18:07:07 +0200

    finish source code parser

--- src/main/kotlin/utils/SourceCodeFetcher.kt
+++ src/main/kotlin/utils/SourceCodeFetcher.kt
@@ -1,8 +1,65 @@
 package org.example.utils
 
-class SourceCodeFetcher {
-    fun fetchMethod(className: String, methodName: String): String?{
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.body.MethodDeclaration
+import io.github.cdimascio.dotenv.dotenv
+import java.io.File
 
-        return null
+class SourceCodeFetcher {
+    fun fetchMethod(className: String, methodName: String): String? {
+        val sourceCode = extractSourceCodeFromFile(className)
+        return sourceCode?.let { extractMethodCode(methodName, it) }
     }
+
+    private fun extractSourceCodeFromFile(className: String): String? {
+        try {
+            val path = getPath(className)
+            val content = path?.let { File(it).readText().trimIndent()  }
+            return content
+        }catch (e: Exception){
+            println(e.message)
+            return null
+        }
+    }
+
+    private fun getPath(className: String): String?{
+        try {
+            val dotenv = dotenv()
+            val basePath = dotenv["SOURCE_CODE"]
+            val res = when (className.first()) {
+                'C' -> basePath + className.replace('.', '/') + ".java"
+                else -> null
+            }
+            return res
+        }catch (e: Exception){
+            println(e.message)
+            return null
+        }
+
+    }
+
+    private fun extractMethodCode(methodName: String, sourceCode: String): String? {
+        try {
+            val parser = JavaParser()
+            val cu: CompilationUnit = parser.parse(sourceCode).result.get()
+
+            // Visit methods in the class
+            cu.findAll(MethodDeclaration::class.java).forEach { method ->
+                if (method.nameAsString == methodName) {
+                    return method.toString()
+                }
+            }
+            return null
+        }catch (e: Exception){
+            println(e.message)
+            return null
+        }
+
+    }
+
 }
+//
+//fun main(){
+//    print(SourceCodeFetcher().fetchMethod("CH.ifa.draw.samples.javadraw.JavaDrawApp", "openViw"))
+//}

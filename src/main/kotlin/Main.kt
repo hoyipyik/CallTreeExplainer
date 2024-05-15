94156fcf03f39b549c778413f4722863c8638692
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-15 18:07:24 +0200

    upgrade main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,22 +2,23 @@ package org.example
 
 import io.github.cdimascio.dotenv.dotenv
 import org.example.model.CallTree
-import org.example.utils.LLMsCaller
-import org.example.utils.SourceCodeFetcher
-import org.example.utils.XmlParser
+import org.example.utils.*
 
 fun main() {
     println("Code Explainer Started 😎")
     val dotenv = dotenv()
     val callTreePath: String = dotenv["CALL_TREE"]
+    val jsonFilePath: String = dotenv["JSON_FILE_PATH"]
 
     val xmlParser = XmlParser()
     val sourceCodeFetcher = SourceCodeFetcher()
     val llMsCaller = LLMsCaller()
+    val jsonWriter = JSONWriter()
 
     val callTree: CallTree = xmlParser.constructCallTreeFromPath(callTreePath)
     callTree.iterateAndUpgradeExplanation(sourceCodeFetcher, llMsCaller)
     // save to json
+    callTree.writeTreeToJson(jsonFilePath, jsonWriter)
     // print the tree
-    print(callTree.printCallTree())
+    // print(callTree.printCallTree())
 }

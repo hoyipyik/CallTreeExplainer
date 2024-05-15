acf852d59c72dfc46f3a61956cd698b7a94b3e99
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-15 21:33:56 +0200

    integrate neo4j

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,12 @@ fun main() {
     val dotenv = dotenv()
     val callTreePath: String = dotenv["CALL_TREE"]
     val jsonFilePath: String = dotenv["JSON_FILE_PATH"]
+    val url: String = dotenv["NEO4J_URL"]
+    val username: String = dotenv["NEO4J_USERNAME"]
+    val password: String = dotenv["NEO4J_PASSWORD"]
 
+    val neo4j = Neo4jService(url, username, password)
+    neo4j.deleteAll()
     val xmlParser = XmlParser()
     val sourceCodeFetcher = SourceCodeFetcher()
     val llMsCaller = LLMsCaller()
@@ -19,6 +24,7 @@ fun main() {
     callTree.iterateAndUpgradeExplanation(sourceCodeFetcher, llMsCaller)
     // save to json
     callTree.writeTreeToJson(jsonFilePath, jsonWriter)
+    callTree.getRootNode()?.let { neo4j.saveCallTree(it) }
     // print the tree
     // print(callTree.printCallTree())
 }

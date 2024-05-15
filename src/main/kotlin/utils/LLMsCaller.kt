6a0cacdeb4f7c9dfb600bf18f5fe7e42d565ca0d
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-15 18:06:56 +0200

    initial LLMsCaller

--- src/main/kotlin/utils/LLMsCaller.kt
+++ src/main/kotlin/utils/LLMsCaller.kt
@@ -1,4 +1,12 @@
 package org.example.utils
 
 class LLMsCaller {
+    fun getAIExplanation(methodCode: String): String?{
+        try {
+            return methodCode
+        }catch (e: Exception){
+            println(e.message)
+            return null
+        }
+    }
 }

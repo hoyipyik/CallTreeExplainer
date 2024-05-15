56769aca7ff458ee9dc4ef186c8f1bde3ded2c0f
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-15 19:18:14 +0200

    add neo4j docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation("com.github.javaparser:javaparser-core:3.25.10")
     // https://mvnrepository.com/artifact/com.google.code.gson/gson
     implementation("com.google.code.gson:gson:2.10.1")
+    // https://mvnrepository.com/artifact/org.neo4j.driver/neo4j-java-driver
+    implementation("org.neo4j.driver:neo4j-java-driver:5.20.0")
 
     testImplementation(kotlin("test"))
 }

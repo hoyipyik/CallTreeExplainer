4caeca0f27be85c01dec822704d3e3dc25876d94
Alex Ho (Yeyi He) <hoyipyik@protonmail.com>
2024-05-16 14:57:06 +0200

    finish basic prompt

--- src/main/kotlin/utils/LLMsCaller.kt
+++ src/main/kotlin/utils/LLMsCaller.kt
@@ -1,12 +1,32 @@
 package org.example.utils
 
+import org.example.domain.ChildNodesExplanation
+
 class LLMsCaller {
-    fun getAIExplanation(methodCode: String): String?{
+    fun getAIExplanation(methodCode: String?, childrenExplanation: List<ChildNodesExplanation>): String?{
         try {
-            return methodCode
+            val prompt: String = generatePrompt(methodCode ?: "", childrenExplanation)
+            val res = fetchResFromAI(prompt)
+            return res
         }catch (e: Exception){
             println(e.message)
             return null
         }
     }
+
+    private fun generatePrompt(sourceCode: String, childrenExplanation: List<ChildNodesExplanation>): String{
+        val extractedChildData: List<String> = childrenExplanation.map { item ->
+            item.methodName + ":\n" + item.explanation
+        }
+        val res = "Please summarise this code in one sentence based on the sourceCode and other information below:\n" +
+                "Here is the source:\n" + sourceCode +
+                        "\nHere is explanation to some method:\n" + extractedChildData.joinToString(separator = "\n") +
+                "\n\nIf there is no source code or explanation to some methods, please just ignore them but still give my summarization based on other information\n"
+        return res
+    }
+
+    private fun fetchResFromAI(prompt: String): String {
+        return prompt
+    }
+
 }
